7be9c5089777f11b26e2b73c6a59d19cb89f9d0c
somya-jangid <somyajangid78@gmail.com>
2025-02-24 02:30:29 +0530

    Add or update src/components/Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <ul>
+                <li>FAQ</li>
+                <li>Help Center</li>
+                <li>Account</li>
+                <li>Media Center</li>
+                <li>Investor Relations</li>
+                <li>Jobs</li>
+                <li>Ways to Watch</li>
+                <li>Terms of Use</li>
+                <li>Privacy</li>
+                <li>Cookie Preferences</li>
+                <li>Corporate Information</li>
+                <li>Contact Us</li>
+                <li>Speed Test</li>
+                <li>Legal Notices</li>
+                <li>Only on Netflix</li>
+            </ul>
+        </footer>
+    );
+}
+
+export default Footer;
